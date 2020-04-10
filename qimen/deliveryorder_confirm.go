a95357390c0071265933dbb98cc6a7b9d659f1a5
yellbuy <19892257@qq.com>
2020-04-10 19:05:37 +0800

    奇门订单回传接口调整

--- qimen/deliveryorder_confirm.go
+++ qimen/deliveryorder_confirm.go
@@ -35,79 +35,58 @@ func (client *Client) DeliveryOrderConfirm(dto *DeliveryOrderConfirmReqDto) (bod
 }
 
 type DeliveryOrderConfirmReqDto struct {
-	XMLName       xml.Name       `xml:"request"`
-	Text          string         `xml:",chardata"`
-	DeliveryOrder *DeliveryOrder `xml:"deliveryOrder"`
-	OrderLines    *OrderLines    `xml:"orderLines"`
+	XMLName       xml.Name           `xml:"request"`
+	Text          string             `xml:",chardata"`
+	DeliveryOrder DeliveryOrder      `xml:"deliveryOrder"`
+	Packages      DeliveryPackages   `xml:"packages"`
+	OrderLines    DeliveryOrderLines `xml:"orderLines"`
 }
+
+type DeliveryPackages struct {
+	Text        string            `xml:",chardata"`
+	PackageList []DeliveryPackage `xml:"package"`
+}
+type DeliveryPackage struct {
+	Text                string `xml:",chardata"`
+	LogisticsCode       string `xml:"logisticsCode"`
+	LogisticsName       string `xml:"logisticsName"`
+	ExpressCode         string `xml:"expressCode"`
+	PackageCode         string `xml:"packageCode"`
+	Length              string `xml:"length"`
+	Width               string `xml:"width"`
+	Height              string `xml:"height"`
+	TheoreticalWeight   string `xml:"theoreticalWeight"`
+	Weight              string `xml:"weight"`
+	Volume              string `xml:"volume"`
+	InvoiceNo           string `xml:"invoiceNo"`
+	PackageMaterialList struct {
+		Text            string `xml:",chardata"`
+		PackageMaterial struct {
+			Text     string `xml:",chardata"`
+			Type     string `xml:"type"`
+			Quantity string `xml:"quantity"`
+		} `xml:"packageMaterial"`
+	} `xml:"packageMaterialList"`
+	Items DeliveryItems `xml:"items"`
+}
+type DeliveryItems struct {
+	Text     string         `xml:",chardata"`
+	ItemList []DeliveryItem `xml:"item"`
+}
+type DeliveryItem struct {
+	Text     string `xml:",chardata"`
+	ItemCode string `xml:"itemCode"`
+	ItemId   string `xml:"itemId"`
+	Quantity int    `xml:"quantity"`
+}
+
 type DeliveryOrder struct {
-	Text                 string `xml:",chardata"`
-	DeliveryRequirements struct {
-		Text   string `xml:",chardata"`
-		Remark string `xml:"remark"`
-	} `xml:"deliveryRequirements"`
-	SenderInfo struct {
-		Text   string `xml:",chardata"`
-		Remark string `xml:"remark"`
-	} `xml:"senderInfo"`
-	ReceiverInfo struct {
-		Text   string `xml:",chardata"`
-		Remark string `xml:"remark"`
-	} `xml:"receiverInfo"`
-	PickerInfo struct {
-		Text   string `xml:",chardata"`
-		Remark string `xml:"remark"`
-	} `xml:"pickerInfo"`
-	OrderLines struct {
-		Text   string `xml:",chardata"`
-		Batchs struct {
-			Text   string `xml:",chardata"`
-			Remark string `xml:"remark"`
-			SnCode string `xml:"snCode"`
-		} `xml:"batchs"`
-		SnCode string `xml:"snCode"`
-		Remark string `xml:"remark"`
-		SnList struct {
-			Text string `xml:",chardata"`
-			Sn   string `xml:"sn"`
-		} `xml:"snList"`
-	} `xml:"orderLines"`
-	Items struct {
-		Text   string `xml:",chardata"`
-		Batchs struct {
-			Text   string `xml:",chardata"`
-			Remark string `xml:"remark"`
-		} `xml:"batchs"`
-		PriceAdjustment struct {
-			Text   string `xml:",chardata"`
-			Remark string `xml:"remark"`
-		} `xml:"priceAdjustment"`
-		Remark string `xml:"remark"`
-	} `xml:"items"`
-	Packages struct {
-		Text                string `xml:",chardata"`
-		PackageMaterialList struct {
-			Text   string `xml:",chardata"`
-			Remark string `xml:"remark"`
-		} `xml:"packageMaterialList"`
-		Items struct {
-			Text   string `xml:",chardata"`
-			Remark string `xml:"remark"`
-		} `xml:"items"`
-		Remark string `xml:"remark"`
-	} `xml:"packages"`
-	RelatedOrders struct {
-		Text   string `xml:",chardata"`
-		Remark string `xml:"remark"`
-	} `xml:"relatedOrders"`
+	Text              string `xml:",chardata"`
 	DeliveryOrderCode string `xml:"deliveryOrderCode"`
 	DeliveryOrderId   string `xml:"deliveryOrderId"`
 	WarehouseCode     string `xml:"warehouseCode"`
 	OrderType         string `xml:"orderType"`
 	Status            string `xml:"status"`
-	LogisticsCode     string `xml:"logisticsCode"`
-	LogisticsName     string `xml:"logisticsName"`
-	ExpressCode       string `xml:"expressCode"`
 	OutBizCode        string `xml:"outBizCode"`
 	ConfirmType       string `xml:"confirmType"`
 	OrderConfirmTime  string `xml:"orderConfirmTime"`
@@ -117,33 +96,69 @@ type DeliveryOrder struct {
 	StorageFee        string `xml:"storageFee"`
 	Invoices          struct {
 		Text    string `xml:",chardata"`
-		Remark  string `xml:"remark"`
-		Header  string `xml:"header"`
-		Amount  string `xml:"amount"`
-		Content string `xml:"content"`
-		Detail  struct {
-			Text  string `xml:",chardata"`
-			Items struct {
-				Text   string `xml:",chardata"`
-				Batchs struct {
-					Text   string `xml:",chardata"`
-					Remark string `xml:"remark"`
-				} `xml:"batchs"`
-				PriceAdjustment struct {
-					Text   string `xml:",chardata"`
-					Remark string `xml:"remark"`
-				} `xml:"priceAdjustment"`
-				Remark   string `xml:"remark"`
-				ItemName string `xml:"itemName"`
-				Unit     string `xml:"unit"`
-				Price    string `xml:"price"`
-				Quantity string `xml:"quantity"`
-				Amount   string `xml:"amount"`
-				ItemCode string `xml:"itemCode"`
-				ItemId   string `xml:"itemId"`
-			} `xml:"items"`
-		} `xml:"detail"`
-		Code   string `xml:"code"`
-		Number string `xml:"number"`
+		Invoice struct {
+			Text    string `xml:",chardata"`
+			Header  string `xml:"header"`
+			Amount  string `xml:"amount"`
+			Content string `xml:"content"`
+			Detail  struct {
+				Text  string `xml:",chardata"`
+				Items struct {
+					Text string `xml:",chardata"`
+					Item struct {
+						Text     string `xml:",chardata"`
+						ItemName string `xml:"itemName"`
+						Unit     string `xml:"unit"`
+						Price    string `xml:"price"`
+						Quantity string `xml:"quantity"`
+						Amount   string `xml:"amount"`
+					} `xml:"item"`
+				} `xml:"items"`
+			} `xml:"detail"`
+			Code   string `xml:"code"`
+			Number string `xml:"number"`
+		} `xml:"invoice"`
 	} `xml:"invoices"`
+}
+type DeliveryOrderLines struct {
+	Text          string              `xml:",chardata"`
+	OrderLineList []DeliveryOrderLine `xml:"orderLine"`
+}
+type DeliveryOrderLine struct {
+	Text            string   `xml:",chardata"`
+	OrderLineNo     string   `xml:"orderLineNo"`
+	OrderSourceCode string   `xml:"orderSourceCode"`
+	SubSourceCode   string   `xml:"subSourceCode"`
+	OwnerCode       []string `xml:"ownerCode"`
+	ItemCode        string   `xml:"itemCode"`
+	ItemId          string   `xml:"itemId"`
+	SnList          struct {
+		Text string `xml:",chardata"`
+		Sn   string `xml:"sn"`
+	} `xml:"snList"`
+	InventoryType  string  `xml:"inventoryType"`
+	ItemName       string  `xml:"itemName"`
+	ExtCode        string  `xml:"extCode"`
+	PlanQty        int     `xml:"planQty"`
+	ActualQty      int     `xml:"actualQty"`
+	RetailPrice    float32 `xml:"retailPrice"`
+	ActualPrice    float32 `xml:"actualPrice"`
+	DiscountAmount float32 `xml:"discountAmount"`
+	BatchCode      string  `xml:"batchCode"`
+	ProductDate    string  `xml:"productDate"`
+	ExpireDate     string  `xml:"expireDate"`
+	ProduceCode    string  `xml:"produceCode"`
+	Batchs         struct {
+		Text  string `xml:",chardata"`
+		Batch struct {
+			Text          string `xml:",chardata"`
+			BatchCode     string `xml:"batchCode"`
+			ProductDate   string `xml:"productDate"`
+			ExpireDate    string `xml:"expireDate"`
+			ProduceCode   string `xml:"produceCode"`
+			InventoryType string `xml:"inventoryType"`
+			ActualQty     string `xml:"actualQty"`
+		} `xml:"batch"`
+	} `xml:"batchs"`
+	QrCode string `xml:"qrCode"`
 }
