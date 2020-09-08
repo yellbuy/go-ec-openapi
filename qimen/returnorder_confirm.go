cb6408ee0504419b311b8d4ba85b289c86d78d69
yellbuy <19892257@qq.com>
2020-09-08 09:52:29 +0800

    奇门退货单接口集成

---
+++ qimen/returnorder_confirm.go
@@ -0,0 +1,42 @@
+// https://qimen.taobao.com/qimen/index.htm#/api-doc?fromPage=api-list&&apiServiceId=&officialApiId=25995&_k=crdysi
+// taobao.qimen.returnorder.confirm 入库单确认接口
+
+package qimen
+
+import (
+	"encoding/xml"
+	"fmt"
+
+	"github.com/yellbuy/go-ec-openapi/common"
+)
+
+// 退货单回传
+func (client *Client) ReturnOrderConfirm(dto *ReturnOrderConfirmReqDto) (body []byte, err error) {
+	var bytes []byte
+	bytes, err = xml.Marshal(dto)
+
+	req := make(map[string]interface{})
+	//fmt.Println("bizcontent：", string(bizcontent))
+	params, resErr := common.InterfaceToParameter(req)
+	if resErr != nil {
+		fmt.Println(resErr)
+		err = resErr
+		body = nil
+		return
+	}
+
+	// 通过奇门代理平台
+	method := "taobao.qimen.returnorder.confirm"
+	body, err = client.Execute(method, params, bytes)
+	if err != nil {
+		fmt.Println(method, err)
+	}
+	return
+}
+
+type ReturnOrderConfirmReqDto struct {
+	XMLName     xml.Name             `xml:"request"`
+	Text        string               `xml:",chardata"`
+	ReturnOrder ReturnOrderCreateDto `xml:"returnOrder"`
+	OrderLines  ReturnOrderLines     `xml:"orderLines"`
+}
---
+++ qimen/returnorder_create.go
@@ -0,0 +1,107 @@
+/*
+文档地址：https://qimen.taobao.com/qimen/index.htm#/api-doc?fromPage=api-list&&apiServiceId=&officialApiId=25991&_k=2c1orl
+*/
+
+package qimen
+
+import (
+	"encoding/xml"
+)
+
+// 退货单下载同步解析
+func ReturnOrderCreateParse(body []byte) (res *ReturnOrderCreateReqDto, err error) {
+	res = new(ReturnOrderCreateReqDto)
+	err = xml.Unmarshal(body, res)
+	return
+}
+
+type ReturnOrderCreateReqDto struct {
+	XMLName     xml.Name              `xml:"request"`
+	ReturnOrder *ReturnOrderCreateDto `xml:"returnOrder"`
+	OrderLines  *OrderLines           `xml:"orderLines"`
+}
+type ReturnOrderCreateDto struct {
+	Text                 string `xml:",chardata"`
+	ReturnOrderCode      string `xml:"returnOrderCode"`
+	OwnerCode            string `xml:"ownerCode"`
+	WarehouseCode        string `xml:"warehouseCode"`
+	OrderType            string `xml:"orderType"`
+	SourcePlatformCode   string `xml:"sourcePlatformCode"`
+	SourcePlatformName   string `xml:"sourcePlatformName"`
+	ShopNick             string `xml:"shopNick"`
+	SellerNick           string `xml:"sellerNick"`
+	OrderFlag            string `xml:"orderFlag"`
+	PreDeliveryOrderCode string `xml:"preDeliveryOrderCode"`
+	PreDeliveryOrderId   string `xml:"preDeliveryOrderId"`
+	LogisticsCode        string `xml:"logisticsCode"`
+	LogisticsName        string `xml:"logisticsName"`
+	ExpressCode          string `xml:"expressCode"`
+	ReturnReason         string `xml:"returnReason"`
+	BuyerNick            string `xml:"buyerNick"`
+	SenderInfo           struct {
+		Text          string `xml:",chardata"`
+		Company       string `xml:"company"`
+		Name          string `xml:"name"`
+		ZipCode       string `xml:"zipCode"`
+		Tel           string `xml:"tel"`
+		Mobile        string `xml:"mobile"`
+		Email         string `xml:"email"`
+		CountryCode   string `xml:"countryCode"`
+		Province      string `xml:"province"`
+		City          string `xml:"city"`
+		Area          string `xml:"area"`
+		Town          string `xml:"town"`
+		DetailAddress string `xml:"detailAddress"`
+	} `xml:"senderInfo"`
+	Remark string `xml:"remark"`
+}
+
+type ReturnOrderLines struct {
+	OrderLineList []*ReturnOrderLine `xml:"orderLine"`
+}
+type ReturnOrderLine struct {
+	OrderLineNo        string `xml:"orderLineNo"`
+	SourceOrderCode    string `xml:"sourceOrderCode"`
+	SubSourceOrderCode string `xml:"subSourceOrderCode"`
+	OwnerCode          string `xml:"ownerCode"`
+	ItemCode           string `xml:"itemCode"`
+	ItemId             string `xml:"itemId"`
+	SnList             struct {
+		Text string `xml:",chardata"`
+		Sn   string `xml:"sn"`
+	} `xml:"snList"`
+	InventoryType string `xml:"inventoryType"`
+	PlanQty       int    `xml:"planQty"`
+	ActualQty     int    `xml:"actualQty"`
+	BatchCode     string `xml:"batchCode"`
+	ProductDate   string `xml:"productDate"`
+	ExpireDate    string `xml:"expireDate"`
+	ProduceCode   string `xml:"produceCode"`
+	Batchs        struct {
+		Text  string `xml:",chardata"`
+		Batch struct {
+			Text          string `xml:",chardata"`
+			BatchCode     string `xml:"batchCode"`
+			ProductDate   string `xml:"productDate"`
+			ExpireDate    string `xml:"expireDate"`
+			ProduceCode   string `xml:"produceCode"`
+			InventoryType string `xml:"inventoryType"`
+			ActualQty     string `xml:"actualQty"`
+		} `xml:"batch"`
+	} `xml:"batchs"`
+	QrCode string `xml:"qrCode"`
+}
+
+type ReturnOrderCreateResponse struct {
+	XMLName xml.Name `xml:"response"`
+	Response
+	ReturnOrderId string `xml:"returnOrderId,omitempty"`
+}
+
+func NewReturnOrderCreateSuccessResponse(returnOrderId string) *ReturnOrderCreateResponse {
+	dto := new(ReturnOrderCreateResponse)
+	dto.Flag = "success"
+	dto.Code = "0"
+	dto.ReturnOrderId = returnOrderId
+	return dto
+}
